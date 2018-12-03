e05d74c395633e7bd2ed932fa4dd3d8ec517c7ab
Joao Junior <jcajcefet@yahoo.com.br>
2018-12-03 15:33:43 +0100

    Use memoization

--- recursive-staircase/main.py
+++ recursive-staircase/main.py
@@ -1,10 +1,14 @@
-def recursive_staircase(available, steps=(1, 2, 3)):
-    if available < 0:
+def recursive_staircase(available, steps=(1, 2, 3), memoization={}):
+    if available in memoization:
+        return memoization[available]
+    elif available < 0:
         return 0
     elif available == 0:
         return 1
     else:
         result = 0
         for step in steps:
-            result += recursive_staircase(available - step)
+            result_aux = recursive_staircase(available - step)
+            memoization[available-step] = result_aux
+            result += result_aux
         return result

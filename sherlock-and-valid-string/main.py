800bbbe1f586efd703c91ef67b45c3d70fdc68f2
Joao Junior <jcajcefet@yahoo.com.br>
2018-12-11 13:46:15 +0100

    Refactor to be more simple

--- sherlock-and-valid-string/main.py
+++ sherlock-and-valid-string/main.py
@@ -1,28 +1,16 @@
 def is_valid(s):
     frequencies = {}
-    min_ = len(s)
-    max_ = 0
-    qty_max = 0
-    qty_min = 0
     result = 'NO'
     for c in s:
         frequencies[c] = frequencies.get(c, 0) + 1
-    for frequency in frequencies.values():
-        if frequency < min_:
-            min_ = frequency
-            qty_min = 1
-        elif frequency == min_:
-            qty_min += 1
-        if frequency > max_:
-            max_ = frequency
-            qty_max = 1
-        elif frequency == max_:
-            qty_max += 1
-    if max_ == min_:
-        result = 'YES'
-    elif (max_ - min_) == 1 and (qty_max == 1 or qty_min == 1):
-        result = 'YES'
-    elif (max_ - min_) >= 2 and (min_ == 1):
-        result = 'YES'
 
+    values = sorted(frequencies.values())
+    min_ = values[0]
+    max_ = values[-1]
+    if min_ == max_:
+        result = 'YES'
+    elif min_ == 1 and max_ == values[1]:
+        result = 'YES'
+    elif min_ == max_ - 1 and min_ == values[-2]:
+        result = 'YES'
     return result

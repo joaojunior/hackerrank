8d2b9dc9fbd758605bf662bce70f1498bbf8b6f0
Joao Junior <jcajcefet@yahoo.com.br>
2019-01-22 21:03:39 +0100

    Recursion version solution

--- arithmetic_expression/main.py
+++ arithmetic_expression/main.py
@@ -1,47 +1,33 @@
-import sys
-sys.setrecursionlimit(10**9)
+class ArithmeticExpression():
+    def find(self, array):
+        self.array = array
+        self.size = len(array)
 
+        result = self._find(1, array[0], [])
+        response = str(array[0])
+        for i, operator in enumerate(result):
+            response += '{0}{1}'.format(operator, array[i+1])
+        return response
 
-class Backtrack():
-    def backtrack(self, numbers, divisor=101):
-        self.divisor = divisor
-        self.numbers = numbers
-        self.signals = ['*', '-', '+']
-        self.size = len(numbers)
-        self.find = False
-        for signal in self.signals:
-            if self.find is False:
-                self._backtrack(0, [signal], numbers[0])
-
-    def _backtrack(self, i, signals, sum_):
-        if sum_ % self.divisor == 0:
-            self.find = True
-            j = 0
-            while j < i:
-                print(self.numbers[j], signals[j+1], sep='', end='')
-                j += 1
-            print(self.numbers[j], end='')
-            sys.exit(0)
+    def _find(self, i, sum_, response):
+        if i < self.size:
+            result = (
+                self._find(i+1, sum_ * self.array[i], response[:] + ['*']) or
+                self._find(i+1, sum_ + self.array[i], response[:] + ['+']) or
+                self._find(i+1, sum_ - self.array[i], response[:] + ['-']))
+            if result is False and self.array[i] != 0:
+                result = self._find(i+1, sum_ / self.array[i-1],
+                                    response[:] + ['/'])
+            return result
         else:
-            if i < self.size - 1 and len(signals) <= 5 * self.size:
-                for signal in self.signals:
-                    if signal == '*':
-                        new_sum = sum_ * self.numbers[i + 1]
-                    elif signal == '-':
-                        new_sum = sum_ - self.numbers[i + 1]
-                    elif signal == '+':
-                        new_sum = sum_ + self.numbers[i + 1]
-                    if self.find is False:
-                        self._backtrack(i + 1, signals[:] + [signal], new_sum)
-                    else:
-                        return
+            if sum_ % 101 == 0:
+                return response
+            else:
+                return False
 
 
 if __name__ == '__main__':
     n = int(input())
-    numbers_raw = input().split()
-    numbers = []
-    for i in range(n):
-        numbers.append(int(numbers_raw[i]))
-    backtrack = Backtrack()
-    backtrack.backtrack(numbers)
+    numbers = list(map(int, input().rstrip().split()))
+    a = ArithmeticExpression()
+    print(a.find(numbers))

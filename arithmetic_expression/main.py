94812e11fb8d9ae6f1ea0c0ddc1753baaa27725f
Joao Junior <jcajcefet@yahoo.com.br>
2019-01-23 21:02:38 +0100

    Improve the time

--- arithmetic_expression/main.py
+++ arithmetic_expression/main.py
@@ -2,9 +2,27 @@ class ArithmeticExpression():
     def find(self, array):
         self.array = array
         self.size = len(array)
-
-        result = self._find(1, array[0], str(array[0]))
-        return result
+        queue = [(array[0], str(array[0]))]
+        i = 1
+        while i < self.size:
+            q = []
+            for j in range(len(queue)):
+                sum_, response = queue[j]
+                sum_plus = sum_ + array[i]
+                sum_times = sum_ * array[i]
+                sum_minus = sum_ - array[i]
+                if sum_ % 101 == 0:
+                    q = [[sum_times, response + '*' + str(array[i])]]
+                    break
+                else:
+                    q.append([sum_plus, response + '+' + str(array[i])])
+                    q.append([sum_times, response + '*' + str(array[i])])
+                    q.append([sum_minus, response + '-' + str(array[i])])
+            queue = q[:]
+            i += 1
+        for sum_, response in queue:
+            if sum_ % 101 == 0:
+                return response
 
     def _find(self, i, sum_, response):
         if i < self.size:

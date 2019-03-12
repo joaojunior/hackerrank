b0aa47dd4d99e5a82b8e2c6f468a532cf5b852ee
Joao Junior <jcajcefet@yahoo.com.br>
2019-03-12 21:20:38 +0100

    Add solution to the problem: Search a 2D Matrix II

---
+++ leetcode/search-a-2d-matrix-ii/main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def search_matrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            if row and (row[0] <= target <= row[-1]):
+                if target in row:
+                    return True
+        return False
